c7b7e9ebf741eec8735cd94f47b5e5bef598b6ce
Siwanper <15091631746@qq.com>
2019-05-15 18:48:25 +0800

    crash

---
+++ CrashHandlerExample/CrashController.h
@@ -0,0 +1,17 @@
+//
+//  CrashController.h
+//  CrashHandlerExample
+//
+//  Created by ios开发 on 2019/5/15.
+//  Copyright © 2019 siwanper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CrashController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CrashHandlerExample/CrashController.m
@@ -0,0 +1,13 @@
+//
+//  CrashController.m
+//  CrashHandlerExample
+//
+//  Created by ios开发 on 2019/5/15.
+//  Copyright © 2019 siwanper. All rights reserved.
+//
+
+#import "CrashController.h"
+
+@implementation CrashController
+
+@end
